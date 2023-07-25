384ce55572c5776a4be0fcc2fb0cd6bfbb0cf7bf
Angelie Kraft <angelie.kraft@tutanota.com>
2023-07-25 15:02:34 +0200

    adding colake, luke, custom gpt2 to StereoSet test

--- experiments/stereoset.py
+++ experiments/stereoset.py
@@ -27,6 +27,8 @@ parser.add_argument(
         "AlbertForMaskedLM",
         "RobertaForMaskedLM",
         "GPT2LMHeadModel",
+        "LukeForMaskedLM",
+        "ColakeForMaskedLM"
     ],
     help="Model to evalute (e.g., BertForMaskedLM). Typically, these correspond to a HuggingFace "
     "class.",
@@ -36,7 +38,8 @@ parser.add_argument(
     action="store",
     type=str,
     default="bert-base-uncased",
-    choices=["bert-base-uncased", "albert-base-v2", "roberta-base", "gpt2"],
+    choices=["bert-base-uncased", "albert-base-v2", "roberta-base", "gpt2", "gpt2-medium", "gpt2-m-kelm", "luke-base",
+             "colake"],
     help="HuggingFace model name or path (e.g., bert-base-uncased). Checkpoint from which a "
     "model is instantiated.",
 )
@@ -59,6 +62,15 @@ parser.add_argument(
 if __name__ == "__main__":
     args = parser.parse_args()
 
+    if "kelm" in args.model_name_or_path:
+        model_name_or_path = "/media/angelie/Samsung_T5/KELM-GPT2/gpt2-medium/kelm_full"
+    elif "colake" in args.model_name_or_path:
+        model_name_or_path = "bias_bench/model/colake"
+    elif "luke" in args.model_name_or_path:
+        model_name_or_path = "studio-ousia/luke-base"
+    else:
+        model_name_or_path = args.model_name_or_path
+
     experiment_id = generate_experiment_id(
         name="stereoset",
         model=args.model,
@@ -69,13 +81,20 @@ if __name__ == "__main__":
     print("Running StereoSet:")
     print(f" - persistent_dir: {args.persistent_dir}")
     print(f" - model: {args.model}")
-    print(f" - model_name_or_path: {args.model_name_or_path}")
+    print(f" - model_name_or_path: {model_name_or_path}")
     print(f" - batch_size: {args.batch_size}")
     print(f" - seed: {args.seed}")
 
-    model = getattr(models, args.model)(args.model_name_or_path)
+
+    model = getattr(models, args.model)(model_name_or_path)
     model.eval()
-    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name_or_path)
+
+    if "kelm" in args.model_name_or_path:
+        tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2-medium")
+    elif "colake" in args.model_name_or_path:
+        tokenizer = transformers.AutoTokenizer.from_pretrained("roberta-base")
+    else:
+        tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name_or_path)
 
     runner = StereoSetRunner(
         intrasentence_model=model,
