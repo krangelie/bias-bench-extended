66aa01faa1cca56402d8c6fb13650a760b24a360
Angelie Kraft <angelie.kraft@tutanota.com>
2023-07-25 15:02:06 +0200

    adding colake, luke, custom gpt2 to CrowS test

--- experiments/crows.py
+++ experiments/crows.py
@@ -27,8 +27,10 @@ parser.add_argument(
         "AlbertForMaskedLM",
         "RobertaForMaskedLM",
         "GPT2LMHeadModel",
+        "LukeForMaskedLM",
+        "ColakeForMaskedLM"
     ],
-    help="Model to evalute (e.g., BertForMaskedLM). Typically, these correspond to a HuggingFace "
+    help="Model to evaluate (e.g., BertForMaskedLM). Typically, these correspond to a HuggingFace "
     "class.",
 )
 parser.add_argument(
@@ -36,7 +38,9 @@ parser.add_argument(
     action="store",
     type=str,
     default="bert-base-uncased",
-    choices=["bert-base-uncased", "albert-base-v2", "roberta-base", "gpt2"],
+    choices=["bert-base-uncased", "albert-base-v2", "roberta-base",
+             "gpt2", "gpt2-medium", "gpt2-m-kelm",
+             "luke-base", "colake"],
     help="HuggingFace model name or path (e.g., bert-base-uncased). Checkpoint from which a "
     "model is instantiated.",
 )
@@ -44,13 +48,22 @@ parser.add_argument(
     "--bias_type",
     action="store",
     default=None,
-    choices=["gender", "race", "religion"],
+    choices=["gender", "race", "religion", "all"],
     help="Determines which CrowS-Pairs dataset split to evaluate against.",
 )
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
+    if "kelm" in args.model_name_or_path:
+        model_name_or_path = "/media/angelie/Samsung_T5/KELM-GPT2/gpt2-medium/kelm_full"
+    elif "colake" in args.model_name_or_path:
+        model_name_or_path = "bias_bench/model/colake"
+    elif "luke" in args.model_name_or_path:
+        model_name_or_path = "studio-ousia/luke-base"
+    else:
+        model_name_or_path = args.model_name_or_path
 
     experiment_id = generate_experiment_id(
         name="crows",
@@ -62,13 +75,18 @@ if __name__ == "__main__":
     print("Running CrowS-Pairs benchmark:")
     print(f" - persistent_dir: {args.persistent_dir}")
     print(f" - model: {args.model}")
-    print(f" - model_name_or_path: {args.model_name_or_path}")
+    print(f" - model_name_or_path: {model_name_or_path}")
     print(f" - bias_type: {args.bias_type}")
 
     # Load model and tokenizer.
-    model = getattr(models, args.model)(args.model_name_or_path)
+    model = getattr(models, args.model)(model_name_or_path)
     model.eval()
-    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name_or_path)
+    if "kelm" in args.model_name_or_path:
+        tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2-medium")
+    elif "colake" in args.model_name_or_path:
+        tokenizer = transformers.AutoTokenizer.from_pretrained("roberta-base")
+    else:
+        tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name_or_path)
 
     runner = CrowSPairsRunner(
         model=model,
